9c7d5b2dafa2fa4d2c15315ce6f704c81a7d6779
Waseem Rkab <rakab.waseem@gmail.com>
2020-02-02 00:35:11 +0200

    changes

---
+++ wwwroot/js/site.js
@@ -0,0 +1,11 @@
+﻿var ReadMeModalShow = false;
+window.ReadMeModal = () => {
+    if (ReadMeModalShow) {
+        ReadMeModalShow = false;
+        return true;
+    }
+    return false;
+}
+$(document).on('hide.bs.modal', '#readMe', () => {
+    ReadMeModalShow = true;
+})
